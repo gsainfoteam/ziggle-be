18cad39eab81eeada8752795209a7f5f64026305
siwonpada <psw041115@gmail.com>
2023-09-23 22:59:54 +0900

    notice migration

--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -12,7 +12,7 @@ import { ConfigService } from '@nestjs/config';
 import { htmlToText } from 'html-to-text';
 import dayjs from 'dayjs';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
+import { FileType, Notice, User } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
@@ -34,17 +34,65 @@ export class NoticeService {
     getAllNoticeQueryDto: GetAllNoticeQueryDto,
     userUuid?: string,
   ) {
-    const result = await this.noticeRepository.getNoticeList(
-      getAllNoticeQueryDto,
-      userUuid,
-    );
+    const result = await this.prismaService.notice.findMany({
+      take: getAllNoticeQueryDto.limit,
+      skip: getAllNoticeQueryDto.offset,
+      include: {
+        author: true,
+        contents: true,
+        files: {
+          where: {
+            type: FileType.IMAGE,
+          },
+        },
+      },
+      orderBy: {
+        deadline:
+          getAllNoticeQueryDto.orderBy === 'deadline' ? 'desc' : undefined,
+        views: getAllNoticeQueryDto.orderBy === 'hot' ? 'desc' : undefined,
+        createdAt:
+          getAllNoticeQueryDto.orderBy === 'recent' ? 'desc' : undefined,
+      },
+      where: {
+        authorId: getAllNoticeQueryDto.my === 'own' ? userUuid : undefined,
+        reminders: {
+          some: {
+            uuid:
+              getAllNoticeQueryDto.my === 'reminders' ? userUuid : undefined,
+          },
+        },
+        contents: {
+          some: {
+            OR: [
+              {
+                body: {
+                  contains: getAllNoticeQueryDto.search,
+                },
+              },
+              {
+                title: {
+                  contains: getAllNoticeQueryDto.search,
+                },
+              },
+            ],
+          },
+        },
+        tags: {
+          some: {
+            name: {
+              in: getAllNoticeQueryDto.tags,
+            },
+          },
+        },
+      },
+    });
     return {
       ...result,
-      list: result.list.map(({ imagesUrl, ...notice }) => ({
+      list: result.map(({ files, ...notice }) => ({
         ...notice,
         author: notice.author.name,
-        imageUrl: imagesUrl?.[0] ? `${this.s3Url}${imagesUrl[0]}` : null,
-        body: htmlToText(notice.body).slice(0, 100),
+        imageUrl: files?.[0].url ? `${this.s3Url}${files[0].url}` : null,
+        body: htmlToText(notice.contents[0].body).slice(0, 100),
       })),
     };
   }
@@ -57,6 +105,7 @@ export class NoticeService {
           contents: true,
           reminders: true,
           author: true,
+          files: true,
         },
         data: { views: { increment: 1 } },
       })
@@ -72,7 +121,7 @@ export class NoticeService {
     return {
       ...noticeInfo,
       author: notice.author.name,
-      imagesUrl: notice.imagesUrl?.map((url) => `${this.s3Url}${url}`),
+      imagesUrl: notice.files?.map((file) => `${this.s3Url}${file.url}`),
       reminder: reminders.some((reminder) => reminder.uuid === user?.uuid),
     };
   }
@@ -81,61 +130,132 @@ export class NoticeService {
     { title, body, deadline, tags, images }: CreateNoticeDto,
     userUUID: string,
   ) {
-    const user = await this.userRepository.findByUserUUID(userUUID);
+    const user = await this.prismaService.user
+      .findUnique({
+        where: { uuid: userUUID },
+      })
+      .catch(() => {
+        throw new NotFoundException(`User with UUID "${userUUID}" not found`);
+      });
     let findTags = null;
     if (tags) {
-      findTags = await this.tagRepository.findTagList(tags);
+      findTags = await this.prismaService.tag.findMany({
+        where: {
+          id: {
+            in: tags,
+          },
+        },
+      });
     }
-    if (!user)
-      throw new NotFoundException(`User with UUID "${userUUID}" not found`);
     if (images) {
       await this.imageService.validateImages(images);
     }
 
-    const notice = await this.noticeRepository.createNotice(
-      user,
-      title,
-      body,
-      deadline,
-      findTags,
-      images,
-    );
-    if (!notice) throw new InternalServerErrorException(`Notice not created`);
+    const notice = await this.prismaService.notice
+      .create({
+        data: {
+          title,
+          author: {
+            connect: user,
+          },
+          contents: {
+            create: {
+              title,
+              body,
+            },
+          },
+          deadline,
+          tags: {
+            connect: findTags,
+          },
+          files: {
+            create: images?.map((image) => ({
+              name: title,
+              type: FileType.IMAGE,
+              url: image,
+            })),
+          },
+        },
+      })
+      .catch(() => {
+        throw new InternalServerErrorException();
+      });
 
-    const tokens = await this.userRepository.getAllFcmTokens();
+    const tokens = await this.prismaService.fcmToken.findMany();
     this.fcmService.postMessage(
       {
         title: '새 공지글',
         body: title,
         imageUrl: images.length === 0 ? undefined : `${this.s3Url}${images[0]}`,
       },
-      tokens,
+      tokens.map(({ token }) => token),
       { path: `/root/article?id=${notice.id}` },
     );
     return this.getNotice(notice.id);
   }
 
-  async addNoticeReminder(id: number, user: User) {
-    const notice = await this.noticeRepository.addNoticeReminder(id, user);
-    return notice;
+  async addNoticeReminder(id: number, user: User): Promise<Notice> {
+    return await this.prismaService.notice.update({
+      where: { id },
+      data: {
+        reminders: {
+          connect: {
+            uuid: user.uuid,
+          },
+        },
+      },
+    });
   }
 
-  async removeNoticeReminder(id: number, user: User) {
-    const notice = await this.noticeRepository.removeNoticeReminder(id, user);
-    return notice;
+  async removeNoticeReminder(id: number, user: User): Promise<Notice> {
+    return await this.prismaService.notice.update({
+      where: { id },
+      data: {
+        reminders: {
+          disconnect: {
+            uuid: user.uuid,
+          },
+        },
+      },
+    });
   }
 
   async deleteNotice(id: number, userUUID: string): Promise<void> {
-    const result = await this.noticeRepository.deleteNotice(userUUID, id);
-    if (!result) {
-      throw new NotFoundException(`Notice with ID "${id}" not found`);
-    }
-    this.imageService.deleteImages(result.imagesUrl);
+    const result = await this.prismaService.notice
+      .delete({
+        where: { id, authorId: userUUID },
+        select: {
+          files: {
+            where: {
+              type: FileType.IMAGE,
+            },
+          },
+        },
+      })
+      .catch(() => {
+        throw new NotFoundException(`Notice with ID "${id}" not found`);
+      });
+    this.imageService.deleteImages(result.files.map(({ url }) => url));
   }
 
   @Cron('0 9 * * *')
   async sendReminderMessage() {
-    const targetNotices = await this.noticeRepository.getReminderTargetList();
+    const targetNotices = await this.prismaService.notice.findMany({
+      where: {
+        deadline: {
+          gte: dayjs().startOf('d').add(1, 'd').toDate(),
+          lte: dayjs().startOf('d').add(2, 'd').toDate(),
+        },
+      },
+      include: {
+        reminders: {
+          include: {
+            fcmTokens: true,
+          },
+        },
+        files: true,
+      },
+    });
     targetNotices.map((notice) => {
       const leftDate = dayjs(notice.deadline)
         .startOf('d')
@@ -144,8 +264,8 @@ export class NoticeService {
         {
           title: `[Reminder] ${leftDate}일 남은 공지가 있어요!`,
           body: `${notice.title} 마감이 ${leftDate}일 남았어요`,
-          imageUrl: notice.imagesUrl?.[0]
-            ? `${this.s3Url}${notice.imagesUrl[0]}`
+          imageUrl: notice.files?.[0].url
+            ? `${this.s3Url}${notice.files[0].url}`
             : undefined,
         },
         notice.reminders
