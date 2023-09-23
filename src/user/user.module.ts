1ce986ae6d577ef75435dc477fcc0df245773c32
siwonpada <psw041115@gmail.com>
2023-09-23 23:09:08 +0900

    feat: migrate user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { UserRepository } from './user.repository';
 import { HttpModule } from '@nestjs/axios';
 import { IdPGuard } from './guard/idp.guard';
 import { IdPStrategy } from './guard/idp.strategy';
 import { AnonymousStrategy } from './guard/anonymous.strategy';
 import { IdpOptionalStrategy } from './guard/idpOptional.strategy';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, PrismaModule],
   providers: [
     UserService,
-    UserRepository,
     IdPGuard,
     IdPStrategy,
     IdpOptionalStrategy,
     AnonymousStrategy,
   ],
   controllers: [UserController],
-  exports: [IdPGuard, UserRepository],
+  exports: [IdPGuard],
 })
 export class UserModule {}
