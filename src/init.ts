69d6d100ffed5605eec1c5b9f3fef78e37709346
2paperstar <lee@paperst.ar>
2023-11-28 19:44:25 +0900

    fix: set default timezone after register timezone plugin

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,6 @@ import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
-dayjs.tz.setDefault('Asia/Seoul');
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Seoul');
