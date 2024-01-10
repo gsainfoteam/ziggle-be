18b3fb98257d3a923b0762ec6d36f2cf16b89823
Shiwon Park <psw041115@gmail.com>
2024-01-10 21:33:32 +0900

    add migration file

---
+++ prisma/migrations/20240110123021_reaction/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE `file` ADD COLUMN `order` INTEGER NOT NULL DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE `Reaction` (
+    `emoji` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `deleted_at` DATETIME(3) NULL,
+    `notice_id` INTEGER NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`emoji`, `notice_id`, `user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Reaction` ADD CONSTRAINT `Reaction_notice_id_fkey` FOREIGN KEY (`notice_id`) REFERENCES `Notice`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Reaction` ADD CONSTRAINT `Reaction_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`uuid`) ON DELETE RESTRICT ON UPDATE CASCADE;
