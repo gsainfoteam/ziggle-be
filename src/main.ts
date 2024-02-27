41b9229379679cdca8c654717c2524fe500c43fd
siwonpada <psw041115@gmail.com>
2024-02-27 17:28:17 +0900

    set swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // set CORS config
+  const whitelist = [
+    /https:\/\/.*ziggle.gistory.me/,
+    /https:\/\/.*ziggle-fe.pages.dev/,
+    /http:\/\/localhost:3000/,
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.some((regex) => regex.test(origin))) {
+        callback(null, origin);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+  // set cookie config
+  app.use(cookieParser());
+  // set swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Ziggle API')
+    .setDescription('Ziggle API')
+    .setVersion('1.0')
+    .addTag('Ziggle')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // start server
   await app.listen(3000);
 }
 bootstrap();
