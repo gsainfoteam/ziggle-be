edfea226dead8e309ebdb59e68910171e3095072
Shiwon Park <psw041115@gmail.com>
2024-03-19 01:32:11 +0900

    add swagger document

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -12,7 +12,13 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { NoticeService } from './notice.service';
 import { IdPGuard, IdPOptionalGuard } from 'src/user/guard/id.guard';
 import { GeneralNoticeListDto } from './dto/res/generalNotice.dto';
@@ -31,6 +37,16 @@ import { UpdateNoticeDto } from './dto/req/updateNotice.dto';
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {}
 
+  @ApiOperation({
+    summary: 'Get notice list',
+    description: 'Get notice list',
+  })
+  @ApiOkResponse({
+    type: GeneralNoticeListDto,
+    description: 'Return notice list',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Get()
   @UseGuards(IdPOptionalGuard)
   async getNoticeList(
@@ -40,6 +56,14 @@ export class NoticeController {
     return this.noticeService.getNoticeList(query, user?.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Get all notice list',
+    description: 'Get all notice list',
+  })
+  @ApiOkResponse({
+    type: GeneralNoticeListDto,
+    description: 'Return all notice list',
+  })
   @Get('all')
   @UseGuards(IdPOptionalGuard)
   async getAllNoticeList(
@@ -49,6 +73,16 @@ export class NoticeController {
     return this.noticeService.getNoticeList(query, user?.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Get notice',
+    description: 'Get notice',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Get(':id')
   @UseGuards(IdPOptionalGuard)
   async getNotice(
@@ -59,6 +93,16 @@ export class NoticeController {
     return this.noticeService.getNotice(id, query, user?.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Create notice',
+    description: 'Create notice',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Post()
   @UseGuards(IdPGuard)
   async createNotice(
@@ -68,6 +112,16 @@ export class NoticeController {
     return this.noticeService.createNotice(createNoticeDto, user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Add additional notice',
+    description: 'Add additional notice',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Post(':id/additional')
   @UseGuards(IdPGuard)
   async createAdditionalNotice(
@@ -82,6 +136,16 @@ export class NoticeController {
     );
   }
 
+  @ApiOperation({
+    summary: 'Add notice reminder',
+    description: 'Add notice reminder',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Post(':id/:contentIdx/foreign')
   @UseGuards(IdPGuard)
   async addForeignContent(
@@ -98,6 +162,16 @@ export class NoticeController {
     );
   }
 
+  @ApiOperation({
+    summary: 'Add notice reminder',
+    description: 'Add notice reminder',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Post(':id/reaction')
   @UseGuards(IdPGuard)
   async addReaction(
@@ -108,6 +182,16 @@ export class NoticeController {
     return this.noticeService.addNoticeReaction(body, id, user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Add notice reminder',
+    description: 'Add notice reminder',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Patch(':id')
   @UseGuards(IdPGuard)
   async updateNotice(
@@ -127,6 +211,16 @@ export class NoticeController {
     return this.noticeService.removeNoticeReminder(id, user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Delete notice reaction',
+    description: 'Delete notice reaction',
+  })
+  @ApiOkResponse({
+    type: ExpandedGeneralNoticeDto,
+    description: 'Return notice',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Delete(':id/reaction')
   @UseGuards(IdPGuard)
   async deleteReaction(
@@ -137,6 +231,13 @@ export class NoticeController {
     return this.noticeService.removeNoticeReaction(body, id, user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'Delete notice',
+    description: 'Delete notice',
+  })
+  @ApiOkResponse({ description: 'Return notice' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
   @Delete(':id')
   @UseGuards(IdPGuard)
   async deleteNotice(
