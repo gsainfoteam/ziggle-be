cc838a18f07d4acc4a1bf7d4f62c4208b0d050d6
siwonpada <psw041115@gmail.com>
2024-05-07 00:11:41 +0900

    add swagger

--- src/crawl/dto/req/getCrawl.dto.ts
+++ src/crawl/dto/req/getCrawl.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GetCrawlDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   url: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
