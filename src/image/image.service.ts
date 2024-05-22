0e3eb9335ffde6118dced7a40f27ba2775fb8a41
2paperstar <lee@paperst.ar>
2024-05-22 15:33:59 +0900

    fix: don't rotate file

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -74,7 +74,7 @@ export class ImageService {
     file: Express.Multer.File,
   ): Promise<Express.Multer.File> {
     this.logger.log('convertToWebp called');
-    file.buffer = await sharp(file.buffer).rotate().webp().toBuffer();
+    file.buffer = await sharp(file.buffer).webp().toBuffer();
     this.logger.log('convertToWebp finished');
     return file;
   }
