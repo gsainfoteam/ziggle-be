827e8355addd591a587c11600e691102cf3844d7
Shiwon Park <psw041115@gmail.com>
2024-09-21 16:52:53 +0900

    hotfix: module

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserService } from 'src/user/user.service';
 import { AiController } from './ai.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [ConfigModule, UserService],
+  imports: [ConfigModule, UserModule],
   providers: [AiService],
   exports: [AiService],
   controllers: [AiController],
