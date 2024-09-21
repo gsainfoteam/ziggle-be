6c5a2db3ee8044fdaa9c90d5ad43e741ce04afac
Shiwon Park <psw041115@gmail.com>
2024-09-21 16:36:01 +0900

    영어공지 자동 번역 mock api 작성 Fixes #93

---
+++ src/ai/ai.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AiService } from './ai.service';
+import { ApiOAuth2, ApiTags } from '@nestjs/swagger';
+import { IdPGuard } from 'src/user/guard/idp.guard';
+import { TranslateDto } from './dto/req/translate.dto';
+import { TranslateResDto } from './dto/res/translateRes.dto';
+
+@ApiTags('ai')
+@ApiOAuth2(['email', 'profile', 'openid'], 'oauth2')
+@UseGuards(IdPGuard)
+@Controller('ai')
+@UsePipes(new ValidationPipe({ transform: true }))
+export class AiController {
+  constructor(private readonly aiService: AiService) {}
+
+  @Post('translate')
+  async translate(
+    @Body() translateDto: TranslateDto,
+  ): Promise<TranslateResDto> {
+    console.log(translateDto);
+    return {
+      text: 'Hello world!',
+      lang: 'en',
+    };
+  }
+}
--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
+import { AiController } from './ai.controller';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, UserService],
   providers: [AiService],
   exports: [AiService],
+  controllers: [AiController],
 })
 export class AiModule {}
