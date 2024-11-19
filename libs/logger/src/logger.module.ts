b769043024ca417e1f2e80ede4f031e292c2c59b
siwonpada <psw041115@gmail.com>
2024-11-19 13:28:34 +0900

    clean: logger

--- libs/logger/src/logger.module.ts
+++ libs/logger/src/logger.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { LoggerService } from './logger.service';
 import { DiscoveryModule } from '@nestjs/core';
 import { LoggerDecoratorRegister } from './logger.decorator.register';
 
 @Module({
   imports: [DiscoveryModule],
-  providers: [LoggerService, LoggerDecoratorRegister],
-  exports: [LoggerService],
+  providers: [LoggerDecoratorRegister],
 })
 export class LoggerModule {}
