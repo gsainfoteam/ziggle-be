d7e79a14ebece773504db80550bdcd55553519d8
siwonpada <psw041115@gmail.com>
2025-01-24 18:26:45 +0900

    update: migration file

--- prisma/migrations/20241228153436_add_last_edited_at/data-migration.ts
+++ prisma/migrations/20241228153436_add_last_edited_at/data-migration.ts
@@ -2,23 +2,30 @@
 // To run this migration, use the following command:
 // npx ts-node ./prisma/migrations/20241228153436_add_last_edited_at/data-migration.ts
 
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$transaction(async (tx) => {
-    const notices = await tx.notice.findMany();
-    for (const notice of notices) {
-      await tx.notice.update({
-        where: { id: notice.id },
-        data: { lastEditedAt: notice.updatedAt, updatedAt: notice.updatedAt },
-      });
-      console.log(
-        `Updated notice ${notice.id} lastEditedAt to ${notice.updatedAt}`,
-      );
-    }
-  });
+  await prisma.$transaction(
+    async (tx) => {
+      const notices = await tx.notice.findMany();
+      for (const notice of notices) {
+        await tx.notice.update({
+          where: { id: notice.id },
+          data: { lastEditedAt: notice.updatedAt, updatedAt: notice.updatedAt },
+        });
+        console.log(
+          `Updated notice ${notice.id} lastEditedAt to ${notice.updatedAt}`,
+        );
+      }
+    },
+    {
+      maxWait: 5000, // default: 2000
+      timeout: 1000000, // default: 5000
+      isolationLevel: Prisma.TransactionIsolationLevel.Serializable, // optional, default defined by database configuration
+    },
+  );
 }
 
 main()
