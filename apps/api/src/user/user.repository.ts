229fb92b35667f365cd0e79f1e2849e22eddc6cd
kshmaker <kshmaker1216@gm.gist.ac.kr>
2025-03-05 20:36:30 +0900

    .catch end

--- apps/api/src/user/user.repository.ts
+++ apps/api/src/user/user.repository.ts
@@ -21,19 +21,41 @@ export class UserRepository {
     uuid,
     name,
   }: Pick<User, 'uuid' | 'name'>): Promise<User> {
-    const user = await this.prismaService.user.findUnique({
-      where: { uuid },
-    });
+    const user = await this.prismaService.user
+      .findUnique({
+        where: { uuid },
+      })
+      .catch((err) => {
+        if (err instanceof PrismaClientKnownRequestError) {
+          this.logger.error('findUserOrCreate error');
+          this.logger.debug(err);
+          throw new InternalServerErrorException('Database error');
+        }
+        this.logger.error('findUserOrCreate error');
+        this.logger.debug(err);
+        throw new InternalServerErrorException('Unknown error');
+      });
     if (user) {
       return user;
     }
-    return this.prismaService.user.create({
-      data: {
-        uuid,
-        name,
-        consent: false,
-      },
-    });
+    return this.prismaService.user
+      .create({
+        data: {
+          uuid,
+          name,
+          consent: false,
+        },
+      })
+      .catch((err) => {
+        if (err instanceof PrismaClientKnownRequestError) {
+          this.logger.error('findUserOrCreate error');
+          this.logger.debug(err);
+          throw new InternalServerErrorException('Database error');
+        }
+        this.logger.error('findUserOrCreate error');
+        this.logger.debug(err);
+        throw new InternalServerErrorException('Unknown error');
+      });
   }
 
   async findUserAndUpdate({
