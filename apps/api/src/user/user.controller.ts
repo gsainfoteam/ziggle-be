950fb1bf87c00e91bba2fdd567d9c5a78198840f
kshmaker <kshmaker1216@gm.gist.ac.kr>
2025-10-13 16:38:25 +0900

    change controller at get user info

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -111,9 +111,9 @@ export class UserController {
   @UseGuards(IdPGuard)
   async getUserInfo(
     @GetIdPUser() userInfo: UserInfo,
-    @GetUser() user: User,
+    //@GetUser() user: User,
   ): Promise<UserInfoRes> {
-    return { ...userInfo, consent: user.consent };
+    return { ...userInfo, consent: true };
   }
 
   @ApiOperation({
