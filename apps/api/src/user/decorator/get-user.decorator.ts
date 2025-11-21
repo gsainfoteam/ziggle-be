34b9bd59b0d0b11b5edd99d7f9628e6f12a44e50
Dohyun Kim <76870940+dohyun06@users.noreply.github.com>
2025-11-21 11:47:27 +0900

    feat: update GetUser decorator to return user.ziggle if available

--- apps/api/src/user/decorator/get-user.decorator.ts
+++ apps/api/src/user/decorator/get-user.decorator.ts
@@ -4,7 +4,7 @@ import { User } from '@prisma/client';
 export const GetUser = createParamDecorator(
   (_data, ctx: ExecutionContext): User | undefined => {
     const req = ctx.switchToHttp().getRequest();
-    console.log(req);
-    return req.user;
+    if (!req.user.ziggle) return req.user;
+    return req.user.ziggle;
   },
 );
