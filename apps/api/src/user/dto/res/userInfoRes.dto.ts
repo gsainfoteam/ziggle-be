d8dc9a70077abade2fdf597f251266e5d10e2a57
Rhseung <ryu@rhseung.me>
2026-03-21 01:58:33 +0900

    refactor: update type declaration

--- apps/api/src/user/dto/res/userInfoRes.dto.ts
+++ apps/api/src/user/dto/res/userInfoRes.dto.ts
@@ -22,7 +22,7 @@ export class UserInfoRes implements UserInfo {
   name: string;
 
   @ApiProperty({
-    type: String,
+    type: 'string | null',
     nullable: true,
     description: 'User profile image',
     example: 'https://bucket.s3.ap-northeast-2.amazonaws.com/1626740269.webp',
