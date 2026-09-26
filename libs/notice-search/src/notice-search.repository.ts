c32516ffb1866b69da1b2bdb8f56c611080e6518
Dohyun Kim <dohyun06b@gmail.com>
2026-09-26 16:35:29 +0900

    refactor: remove backfill module and service, update related imports

--- libs/notice-search/src/notice-search.repository.ts
+++ libs/notice-search/src/notice-search.repository.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@generated/prisma/client';
+import { Loggable } from '@lib/logger/decorator/loggable';
 
 @Injectable()
+@Loggable()
 export class NoticeSearchRepository {
   async getSource(noticeId: number, tx: Prisma.TransactionClient) {
     return tx.notice.findUniqueOrThrow({
